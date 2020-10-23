3d3abe4c0b15e6fb67c9ad3af6fbfeb248e61a21
williycole <64283810+williycole@users.noreply.github.com>
2020-10-23 16:09:03 -0500

    added in computer random choice with character movement

--- script.js
+++ script.js
@@ -1,51 +1,80 @@
-//////////////////////Dont Touch this code- this is the safe playground section
-console.log(`----------------safe playground code, don't touch`);
-//-----------------------------------------CHOICE ARRAY
-//array for both user and cpu choices
-let choiceArray = ['Boom','Rock', 'Paper', 'Scissors'];
-//  console.log(choiceArray[0]); // boom check
-//-----------------------------------------------------
+// //////////////////////Dont Touch this code- this is the safe playground section
+// console.log(`----------------safe playground code, don't touch`);
+// //-----------------------------------------CHOICE ARRAY
+// //array for both user and cpu choices
+// let choiceArray = ['Boom','Rock', 'Paper', 'Scissors'];
+// //  console.log(choiceArray[0]); // boom check
+// //-----------------------------------------------------
 
-////------------------------------------CPU CHOICE CODE
-let cpuChoice = (Math.ceil(Math.random(1)*3));
-	console.log(cpuChoice);
+// ////------------------------------------CPU CHOICE CODE
+// let cpuChoice = (Math.ceil(Math.random(1)*3));
+// 	console.log(cpuChoice);
 
-////cpu choice helper function-- MAYBE MAKE HELPER FUNCTION
-console.log(choiceArray[cpuChoice]);
-////---------------------------------------------------
-console.log(`--------------------------------------------------`);
-////
-////
-////
-////
-////------------------------------------character movement proto type
-//// let golemBtn = document.querySelector('.golem-attack-button');
-//
-//// golemBtn.addEventListener('click', () => {
-////     let golemMove = document.querySelector('.golem-img');
-////     console.log(`--------------------------------------------------`);
-////     console.log('it works');
-////     golemMove.style.paddingBottom = '20rem';
-////     console.log(`--------------------------------------------------`);
-//// });
-//////////////////////Dont Touch this code- this is the safe playground section
+// ////cpu choice helper function-- MAYBE MAKE HELPER FUNCTION
+// console.log(choiceArray[cpuChoice]);
+// ////---------------------------------------------------
+// console.log(`--------------------------------------------------`);
+// ////
+// ////
+// ////
+// ////
+// ////------------------------------------character movement proto type
+// //// let golemBtn = document.querySelector('.golem-attack-button');
+// //
+// //// golemBtn.addEventListener('click', () => {
+// ////     let golemMove = document.querySelector('.golem-img');
+// ////     console.log(`--------------------------------------------------`);
+// ////     console.log('it works');
+// ////     golemMove.style.paddingBottom = '20rem';
+// ////     console.log(`--------------------------------------------------`);
+// //// });
+// //////////////////////Dont Touch this code- this is the safe playground section
 
 // function userChoice(){
 // }
 // function gameMechanics(){
 // }
 
-
-
-
-
-
-
-
 ////-----------------------------------I am currently working below here
 
+let choiceArray = ['Boom','Rock', 'Paper', 'Scissors'];
+let golemBtn = document.querySelector('.golem-attack-button');
+
+golemBtn.addEventListener('mousedown', () => {
+    let golemMove = document.querySelector('.golem-img');
+    golemMove.style.paddingBottom = '20rem';
+    ////make computers choice pic in here as well and move
+    let cpuChoice = (Math.ceil(Math.random(1)*3));
+	console.log(choiceArray[cpuChoice]);
+});
+
+golemBtn.addEventListener('mouseup', () => {
+    let golemMove = document.querySelector('.golem-img');
+    golemMove.style.paddingBottom = '0rem';
+    ////decide in here who won and update, and make computer choice move back
+});
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////-----------------------------code ideas below here, don't worry about it unless you get stuck
 // let golemBtn = document.querySelector('.golem-attack-button');
 // golemBtn.addEventListener('click', () => {
 
@@ -63,19 +92,3 @@ console.log(`--------------------------------------------------`);
 //               console.log('retreat, it works')
 //         }
 // });
-
-
-let golemBtn = document.querySelector('.golem-attack-button');
-
-golemBtn.addEventListener('mousedown', () => {
-    let golemMove = document.querySelector('.golem-img');
-    golemMove.style.paddingBottom = '20rem';
-    ////make computers choice pic in here as well and move
-    console.log(choiceArray[cpuChoice]);
-});
-
-golemBtn.addEventListener('mouseup', () => {
-    let golemMove = document.querySelector('.golem-img');
-    golemMove.style.paddingBottom = '0rem';
-    ////decide in here who won and update, and make computer choice move back
-});
