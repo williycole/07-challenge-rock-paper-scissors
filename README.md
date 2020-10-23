40e4a3f736a053844085ef6acd6d4ce06c0801cd
williycole <64283810+williycole@users.noreply.github.com>
2020-10-23 12:54:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 07-challenge-rock-paper-scissors
+ 
---
+++ playground/playground.js
@@ -0,0 +1,48 @@
+//////////////////////Dont Touch this code- this is the safe playground section
+console.log(`----------------safe playground code, don't touch`);
+//-----------------------------------------CHOICE ARRAY
+//array for both user and cpu choices
+let choiceArray = ['Boom','Rock', 'Paper', 'Scissors'];
+//  console.log(choiceArray[0]); // boom check
+//-----------------------------------------------------
+
+////------------------------------------CPU CHOICE CODE
+let cpuChoice = (Math.ceil(Math.random(1)*3));
+	console.log(cpuChoice);
+
+////cpu choice helper function-- MAYBE MAKE HELPER FUNCTION
+console.log(choiceArray[cpuChoice]);
+////---------------------------------------------------
+console.log(`--------------------------------------------------`);
+//////////////////////Dont Touch this code- this is the safe playground section
+
+// function userChoice(){
+// }
+// function gameMechanics(){
+// }
+
+
+
+
+
+
+
+
+////-----------------------------------I am currently working below here
+
+function userCharacterMove() {
+let golemAtkBtn = document.getElementsByClassName('golem-attack-button');
+var golemMove =document.getElementsByClassName('golem-move');
+
+let archerAtkBtn = document.getElementsByClassName('archer-attack-button');
+let knightAtkBtn = document.getElementsByClassName('knight-attack-button');
+let ninjaAtkBtn = document.getElementsByClassName('ninja-attack-button');
+
+	if(golemMove.style.paddingBottom = "0px"){
+		console.log('it works')
+	}else {
+		console.log('it doesnt work')
+	}
+
+}
+
